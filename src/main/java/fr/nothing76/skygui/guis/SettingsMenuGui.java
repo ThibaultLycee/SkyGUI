8d59b34d7a9c62db36eb662ac70697dad928fdcc
Thibault Durand <thib.durand@free.fr>
2021-11-11 15:58:22 +0100

    Added the rendering of a single slider

--- src/main/java/fr/nothing76/skygui/guis/SettingsMenuGui.java
+++ src/main/java/fr/nothing76/skygui/guis/SettingsMenuGui.java
@@ -1,6 +1,10 @@
 package fr.nothing76.skygui.guis;
 
+import org.lwjgl.opengl.GL11;
+
+import fr.nothing76.skygui.resources.TexturesPath;
 import fr.nothing76.skygui.utils.SkyGuiEnums;
+import fr.nothing76.skygui.utils.Utils;
 import net.minecraft.client.gui.GuiScreen;
 
 public class SettingsMenuGui extends GuiScreen{
@@ -8,6 +12,17 @@ public class SettingsMenuGui extends GuiScreen{
 	public SettingsMenuGui() {
 		for (SkyGuiEnums.SettingBool setting : SkyGuiEnums.SettingBool.values()) {
 			System.out.println(setting.getValue());
+		}
+	}
+	
+	@Override
+	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+		for (SkyGuiEnums.SettingBool setting : SkyGuiEnums.SettingBool.values()) {
+			if (setting.getValue()) {
+				Utils.drawTexturedRect(50, 50, 76, 32, 0f, 1f, .5f, 1f, GL11.GL_NEAREST, TexturesPath.ASSET_SLIDER);
+			} else {
+				Utils.drawTexturedRect(50, 50, 76, 32, 0f, 1f, 0f, .5f, GL11.GL_NEAREST, TexturesPath.ASSET_SLIDER);
+			}
 		}
 	}
 	
